21f8b1bffb68a6bdb93c2a0b54937724449fd402
Israelam72 <israelarantes.m@gmail.com>
2025-05-03 01:08:30 -0300

    Rename project from "movie_rental" to "rentamovie" and set up new Django backend structure
    - Updated .gitignore to reflect the new project name.
    - Modified docker-compose.yml to change database container and database name.
    - Adjusted manage.py to point to the new settings module.
    - Created new backend directory with initial Django files including settings, URLs, WSGI, and ASGI configurations.
    - Ensured environment variables are used for sensitive settings in settings.py.

--- rentamovie_backend/asgi.py
+++ rentamovie_backend/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for movie_rental project.
+ASGI config for rentamovie_backend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movie_rental.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rentamovie_backend.settings')
 
 application = get_asgi_application()
--- rentamovie_backend/wsgi.py
+++ rentamovie_backend/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for movie_rental project.
+WSGI config for rentamovie_backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movie_rental.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rentamovie_backend.settings')
 
 application = get_wsgi_application()
