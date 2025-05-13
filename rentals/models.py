f0155cbb9e76976d8c696806842c6633496b9a1d
Israelam72 <israelarantes.m@gmail.com>
2025-05-12 23:51:51 -0300

    Add plans and subscriptions models, serializers, and admin registration
    - Introduced Plans and Subscriptions models to manage customer plans and subscriptions.
    - Created corresponding serializers for Plans and Subscriptions.
    - Registered Plans and Subscriptions in the Django admin interface for easier management.
    - Added a new view to retrieve all plans and updated URL patterns to include the plans endpoint.

--- rentals/models.py
+++ rentals/models.py
@@ -18,3 +18,32 @@ class Rental(models.Model):
 
     def is_available(self) -> bool:
         return not Rental.objects.filter(movie=self, return_date__isnull=True).exists()
+
+
+class Plans(models.Model):
+    """Represents a plan for a customer"""
+    name: models.CharField = models.CharField()
+    price: models.FloatField = models.FloatField()
+    description: models.TextField = models.TextField()
+    duration: models.IntegerField = models.IntegerField()
+    features: models.JSONField = models.JSONField(default=list)
+
+
+class Subscriptions(models.Model):
+    """Represents a subscription of a customer"""
+    customer: models.ForeignKey = models.ForeignKey(
+        User, on_delete=models.PROTECT)
+    plan: models.ForeignKey = models.ForeignKey(
+        Plans, on_delete=models.PROTECT)
+    start_date: models.DateTimeField = models.DateTimeField(auto_now_add=True)
+    end_date: models.DateTimeField = models.DateTimeField(
+        null=True, blank=True)
+    is_active: models.BooleanField = models.BooleanField(default=True)
+    created_at: models.DateTimeField = models.DateTimeField(auto_now_add=True)
+    updated_at: models.DateTimeField = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"{self.customer.username} - {self.plan.name} - {self.start_date} - {self.end_date}"
+
+    def __repr__(self) -> str:
+        return f"{self.customer.username} - {self.plan.name}"
